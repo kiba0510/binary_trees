2b0b36bce2bbae848765261c0f6d82ab2cb1e724
kiba0510 <kiba0510@gmail.com>
2021-03-08 11:54:38 -0500

    Added gitignore file and task 0 file

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - 
+ * @parent: Pointer to parent node. 
+ * @value: Value stored in the new node.
+ * 
+ * Return: Pointer to the new node (leaf) or NULL on failure.
+ **/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *root;
+
+    root = malloc(sizeof(binary_tree_t));
+    if (!root)
+        return (NULL);
+    root->parent = parent;
+    root->right = NULL;
+    root->left = NULL;
+    root->n = value;
+
+    return (root);
+}
---
+++ binary_trees.h
@@ -0,0 +1,41 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
+
+/* -- Prototype for Binary Tree Search -- */
+typedef struct binary_tree_s bst_t;
+
+/* -- Prototype for AVL Tree -- */
+typedef struct binary_tree_s avl_t;
+
+/* -- Prototype for Max Binary Heap -- */
+typedef struct binary_tree_s heap_t;
+
+/* -- Prototype for printing a binary tree -- */
+void binary_tree_print(const binary_tree_t *);
+
+/* -- Prototypes for mandatory tasks -- */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+
+#endif /* _BINARY_TREES_H_ */
