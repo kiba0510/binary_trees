bcc5e3432cda7fc41e808b18b7c0e644835dc8ac
kiba0510 <kiba0510@gmail.com>
2021-03-10 21:01:25 +0000

    betty hotfix

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,10 +6,17 @@
  **/
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree)
+	if (!tree)
 	{
-		binaty_tree_delete(tree->right);
-		binaty_tree_delete(tree->left);
-		free(tree);
+		return;
 	}
+	if (tree->left)
+	{
+		binary_tree_delete(tree->left);
+	}
+	if (tree->right)
+	{
+		binary_tree_delete(tree->right);
+	}
+	free(tree);
 }
