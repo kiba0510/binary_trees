4a90ca10bfded84364026637b25c8623de2230d4
kiba0510 <kiba0510@gmail.com>
2021-03-10 10:04:45 -0500

    hotfix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,15 +10,15 @@
  **/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *root = NULL;
+    binary_tree_t *node = NULL;
 
-    root = malloc(sizeof(binary_tree_t));
-    if (root == NULL)
+    node = malloc(sizeof(binary_tree_t));
+    if (node == NULL)
         return (NULL);
-    root->parent = parent;
-    root->right = NULL;
-    root->left = NULL;
-    root->n = value;
+    node->parent = parent;
+    node->right = NULL;
+    node->left = NULL;
+    node->n = value;
 
-    return (root);
+    return (node);
 }
