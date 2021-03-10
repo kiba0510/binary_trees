295ffcaebae1d295177dac6d5f65672c092c1ab0
kiba0510 <kiba0510@gmail.com>
2021-03-10 21:24:39 +0000

    betty hotfix

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,14 +7,14 @@
  **/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t leaves = 0;
+	size_t leaves = 0;
 
-    if (tree)
-    {
-        if (!tree->left && !tree->right)
-            return (1);
-        return (binary_tree_leaves(tree->left) +
-                binary_tree_leaves(tree->right));
-    }
-    return (leaves);
+	if (tree)
+	{
+		if (!tree->left && !tree->right)
+			return (1);
+		return (binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right));
+	}
+	return (leaves);
 }
